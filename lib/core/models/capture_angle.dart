a4841b70bab9dce4d130cfd26013f6504f812386
Developrimbor <bymrglmz@gmail.com>
2025-11-12 20:47:28 +0300

    Update CaptureAngle enum with improved instructions for photo capture angles, enhancing clarity and user guidance.

--- lib/core/models/capture_angle.dart
+++ lib/core/models/capture_angle.dart
@@ -1,10 +1,30 @@
 /// Fotoğraf çekim açıları
 enum CaptureAngle {
-  frontFace('Ön Yüz', 'Kameraya doğrudan bakın', 1),
-  leftSide('Sol Yüz', 'Kafanızı sola çevirin', 2),
-  rightSide('Sağ Yüz', 'Kafanızı sağa çevirin', 3),
-  topVertex('Tepe (Vertex)', 'Telefonu kafanızın üstüne kaldırın', 4),
-  backDonor('Arka Donör', 'Telefonu kafanızın arkasına alın', 5);
+  frontFace(
+    'Ön Yüz',
+    'Lütfen yüzünüzü tam karşıdan iyi bir ışık alacak şekilde fotoğraf çekimini yapınız.',
+    1,
+  ),
+  leftSide(
+    'Sol Taraf',
+    'Lütfen yüzünüzün sol tarafını tam karşıdan alacak şekilde fotoğraf çekimini yapınız.',
+    2,
+  ),
+  rightSide(
+    'Sağ Taraf',
+    'Lütfen yüzünüzün sağ tarafını tam karşıdan alacak şekilde fotoğraf çekimini yapınız.',
+    3,
+  ),
+  topVertex(
+    'Üst Taraf',
+    'Lütfen kafanızın tam olarak üst kısmı görünecek şekilde fotoğraf çekimini yapınız.',
+    4,
+  ),
+  backDonor(
+    'Arka Taraf',
+    'Lütfen kafanızın arka kısmının doğru ışık altında fotoğraf çekimini yapınız.',
+    5,
+  );
 
   final String name;
   final String instruction;
